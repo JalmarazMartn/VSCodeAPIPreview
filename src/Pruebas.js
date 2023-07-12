fc0e3053ddd0998d800e710b409ea6256f3a5c41
Jesus Almaraz martín <JalmarazMartn@gmail.com>
2023-07-12 20:00:25 +0200

    CodeActions.js

--- src/Pruebas.js
+++ src/Pruebas.js
@@ -82,76 +82,8 @@ async function ExecuteCommWithUriAndPos(CommandToExec = '') {
     }
 }
 async function GetCodeActionProvider() {
-    const noCodActionsErrLabel = 'There are no available Code Actions in selection.';
-    const actualRange = new vscode.Range(vscode.window.activeTextEditor.selection.start,
-        vscode.window.activeTextEditor.selection.end);
-    const startRange = new vscode.Range(vscode.window.activeTextEditor.selection.start,
-        vscode.window.activeTextEditor.selection.start);
-
-    let codeActions = await vscode.commands.executeCommand("vscode.executeCodeActionProvider", vscode.window.activeTextEditor.document.uri,
-        actualRange);
-    let codeActionsStart = await vscode.commands.executeCommand("vscode.executeCodeActionProvider", vscode.window.activeTextEditor.document.uri,
-        startRange);
-    if (codeActionsStart) {
-        for (let index = 0; index < codeActionsStart.length; index++) {
-            pushCodeActionsIfNotExists(codeActionsStart[index],codeActions);
-            //codeActions.push(codeActionsStart[index]);
-        }
-    }
-    if (!codeActions) {
-        vscode.window.showErrorMessage(noCodActionsErrLabel);
-        return
-    }
-    if (codeActions.length == 0) {
-        vscode.window.showErrorMessage(noCodActionsErrLabel);
-        return
-    }
-    let codeActionsTitles = [];
-    for (let index = 0; index < codeActions.length; index++) {        
-        codeActionsTitles.push(codeActions[index].title);
-    }
-    console.log(codeActions);
-
-        const codeActionTitle = await vscode.window.showQuickPick(codeActionsTitles,
-            { placeHolder: 'Choose CodeActions to execute.' });
-        if (codeActionTitle == '') {
-            return;
-        }
-        codeActions = codeActions.filter(x => x.title == codeActionTitle);
-        execCodeAction(codeActions);
-}
-function pushCodeActionsIfNotExists(codeActionStart,codeActions)
-{
-    const existingCodeActions = codeActions.filter(x => x.title == codeActionStart.title);
-    if (existingCodeActions)
-    {
-        if (existingCodeActions.length > 0)
-        {
-            return;
-        }
-    }
-    codeActions.push(codeActionStart);
-}
-async function execCodeAction(codeActions) {
-    console.log(codeActions);
-    console.log(codeActions[0].command.command);
-    console.log(codeActions[0].command.arguments);
-    let executionsWithArgs = 'vscode.commands.executeCommand(codeActions[0].command.command';
-    if (codeActions[0].command.arguments) {
-        for (let index = 0; index < codeActions[0].command.arguments.length; index++) {
-            executionsWithArgs = executionsWithArgs + ',codeActions[0].command.arguments[' + index.toString() + ']';
-        }
-        executionsWithArgs = executionsWithArgs + ');';
-    }
-    await eval(executionsWithArgs);
-    /*await vscode.commands.executeCommand(CodeActions[0].command.command,
-        CodeActions[0].command.arguments[0],
-        CodeActions[0].command.arguments[1],
-        CodeActions[0].command.arguments[2],
-        CodeActions[0].command.arguments[3],
-        CodeActions[0].command.arguments[4]
-        );*/
-
+    const codeActions = require('./codeActions.js');
+    codeActions.getCodeActionProvider();
 }
 
 function GetExtensionConf() {
@@ -348,30 +280,6 @@ async function executeDefinitionProvider() {
     const position = vscode.window.activeTextEditor.selection.active;
     const definition = await vscode.commands.executeCommand('vscode.executeDefinitionProvider', document.uri, position);
     console.log(definition);
-}
-async function GetCodeActionsFromDoc() {
-    const document = vscode.window.activeTextEditor.document;
-    const range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(document.lineCount, 0));
-    const definition = await vscode.commands.executeCommand('vscode.executeCodeActionProvider', document.uri, range);
-    //console.log(definition);
-    for (let i = 0; i < definition.length; i++) {
-        const CodeAction = definition[i];
-        console.log(CodeAction);
-    }
-}
-async function GetCodeActionsFromDocByLine() {
-    const document = vscode.window.activeTextEditor.document;
-    //console.log(definition);
-    for (let i = 0; i < document.lineCount; i++) {
-        const range = new vscode.Range(new vscode.Position(i, 0), new vscode.Position(i, 1000));
-        const definition = await vscode.commands.executeCommand('vscode.executeCodeActionProvider', document.uri, range);
-        if (definition) {
-            if (definition.length !== 0) {
-                console.log(i);
-                console.log(definition);
-            }
-        }
-    }
 }
 
 function getProblemMessageCode(problemCode) {
